daf4a1d4fad0f230b347f8801ad645b3524a57cd
ZakariyyaScavotto <zakscav4@gmail.com>
2025-10-16 17:07:30 -0400

    Pushing boilerplate to set it up

---
+++ app.js
@@ -0,0 +1,30 @@
+import express from "express";
+import session from "express-session";
+import configRoutes from "./routes/index.js";
+import exphbs from "express-handlebars";
+
+const app = express();
+
+app.use(
+	session({
+		name: "AuthenticationState",
+		secret: "some secret string!",
+		resave: false,
+		saveUninitialized: false
+	})
+);
+
+app.use("/public", express.static("public"));
+app.use("/static", express.static("static"));
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.engine("handlebars", exphbs.engine({ defaultLayout: "main" }));
+app.set("view engine", "handlebars");
+
+configRoutes(app);
+
+// Start the server
+app.listen(3000, () => {
+	console.log("We've now got a server!");
+	console.log("Your routes will be running on http://localhost:3000");
+});
---
+++ routes/index.js
@@ -0,0 +1,13 @@
+import auth_routes from "./auth_routes.js";
+import { static as staticDir } from "express";
+
+const constructorMethod = (app) => {
+	app.use("/", auth_routes);
+	app.use("/public", staticDir("public"));
+	// per announcement
+	app.use(/(.*)/, (req, res) => {
+		res.status(404).json({ error: "Not found" });
+	});
+};
+
+export default constructorMethod;
