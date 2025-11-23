8fdae9513f9e6c1c8702204f0d18276b00330913
Jordan-Tam <jtam103@gmail.com>
2025-11-23 14:49:31 -0500

    Data functions for announcements/posts

---
+++ data/posts.js
@@ -0,0 +1,126 @@
+import { ObjectId } from "mongodb";
+import { groups } from "../config/mongoCollections.js";
+import usersData from "./users.js";
+import groupsData from "./groups.js";
+import {
+    checkString,
+    checkId
+} from "../helpers.js";
+
+const exportedMethods = {
+
+    async createPost(group_ObjectId, user_ObjectId, title, body) {
+
+        // Input validation.
+        group_ObjectId = checkId(group_ObjectId);
+        user_ObjectId = checkId(user_ObjectId);
+        title = checkString(title, "Title");
+        body = checkString(body, "Body");
+
+        // Check that the group exists.
+        await groupsData.getGroupByID(group_ObjectId);
+
+        // Check that the user exists.
+        let poster = await usersData.getUserById(user_ObjectId);
+
+        // Create the post object.
+        const newPost = {
+            _id: new ObjectId(),
+            poster: poster.userId,
+            title,
+            body
+        };
+
+        const groupsCollection = await groups();
+
+        const updateResult = await groupsCollection.findOneAndUpdate(
+            {_id: new ObjectId(group_ObjectId)},
+            {$push: { posts: newPost }},
+            {returnDocument: "after"}
+        );
+
+        if (!updateResult) {
+            throw "Post could not be created.";
+        }
+
+        return newPost;
+
+    },
+
+    async editPost(post_ObjectId, newTitle, newBody) {
+
+        // Input validation.
+        post_ObjectId = checkId(post_ObjectId);
+        newTitle = checkString(newTitle, "Title");
+        newBody = checkString(newBody, "Body");
+
+        const groupsCollection = await groups();
+
+        const group = await groupsCollection.findOne(
+            {"posts._id": new ObjectId(post_ObjectId)}
+        );
+
+        if (!group) {
+            throw "Post could not be found.";
+        }
+
+        // Update the post from the group's posts array.
+        const groupPosts = group.posts;
+        for (let i = 0; i < groupPosts.length; i++) {
+            if (groupPosts[i]._id.toString() === post_ObjectId.toString()) {
+                groupPosts[i].title = newTitle;
+                groupPosts[i].body = newBody;
+            }
+        }
+
+        const updateInfo = await groupsCollection.findOneAndUpdate(
+            {_id: new ObjectId(group._id)},
+            {$set: {posts: groupPosts}},
+            {returnDocument: "after"}
+        );
+
+        if (!updateInfo) {
+            throw "Post could not be updated.";
+        }
+
+        return true;
+
+    },
+
+    async deletePost(post_ObjectId) {
+
+        // Input validation.
+        post_ObjectId = checkId(post_ObjectId);
+
+        const groupsCollection = await groups();
+
+        const group = await groupsCollection.findOne(
+            {"posts._id": new ObjectId(post_ObjectId)}
+        );
+
+        if (!group) {
+            throw "Post could not be found.";
+        }
+
+        // Remove the post from the group's posts array.
+        const groupPosts = group.posts.filter((post) => {
+            post._id.toString() !== post_ObjectId.toString()
+        });
+
+        const deleteInfo = await groupsCollection.findOneAndUpdate(
+            {_id: new ObjectId(group._id)},
+            {$set: {posts: groupPosts}},
+            {returnDocument: "after"}
+        );
+
+        if (!deleteInfo) {
+            throw "Post could not be deleted.";
+        }
+
+        return true;
+
+    },
+
+};
+
+export default exportedMethods;
